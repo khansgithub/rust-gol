3870ef6278264b0b325ad69c15f95e0b05994557
Khan <khan.ahmed@city.ac.uk>
2024-04-27 01:25:55 +0100

    rewrite gird and print

--- src/grid.rs
+++ src/grid.rs
@@ -1,21 +1,25 @@
 use std::fmt;
 
-#[derive(Default)]
-pub struct Grid{}
+pub struct Grid {
+    arr: Vec<Vec<bool>>,
+}
 
 impl Grid {
-    pub fn new() -> Self {
-        return Self{};
-    }
-
-    fn default() -> Self{
-        return Self{};
+    pub fn new(n: &usize) -> Self {
+        let arr: Vec<Vec<bool>> = vec![vec![false; *n]; *n];
+        return Self { arr };
     }
 }
 
-
-impl fmt::Display for Grid{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
-        write!(f, "test")
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.arr{
+            for cell in row{
+                write!(f, "{}",  if *cell {1} else {0});
+                write!(f, " ");
+            }
+            writeln!(f, " ");
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 #![allow(unused_mut)]
 
 use std::fmt;
+use std::io::{self, Write};
 use std::ptr::null;
 use std::thread;
 use std::time::Duration;
-use std::io::{self, Write};
 
 use ansi_codes::{
     ansi::{hide_cursor, set_cursor_position, show_cursor},
@@ -20,14 +20,10 @@ use grid::Grid;
 // Define a constant for the array size
 const ARRAY_SIZE: usize = 3;
 
-
-
 fn main() {
     clear();
-    let mut grid: Grid = Grid::default();
+    let mut grid: Grid = Grid::new(&ARRAY_SIZE);
     print!("{}", grid);
 }
 
-fn progress_life(grid: &mut Grid) {
-
-}
+fn progress_life(grid: &mut Grid) {}
