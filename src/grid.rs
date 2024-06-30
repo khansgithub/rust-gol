6317ee316fceeae14f4f9beeed6e2b2876ba2dfa
Khan <khan.ahmed@city.ac.uk>
2024-06-30 03:01:05 +0100

    too tired to write a meaningful commit messge

--- src/grid.rs
+++ src/grid.rs
@@ -1,8 +1,11 @@
-use std::{cell, collections::btree_map::Range, fmt};
+use std::{fmt, fs::copy, iter};
+
+use crate::X;
 
 #[derive(Debug)]
 pub struct Grid {
     arr: Vec<Vec<bool>>,
+    // update_arr: *mut Vec<[usize; 3]>,
     size: usize,
     // coordinates: Vec<&_>
 }
@@ -12,9 +15,13 @@ impl Grid {
         if *n < 1 {
             return Err(format!("Input size is malformed: n = {}", n));
         }
+
+        // set up a 2d vec filled wtih `false`
         let mut arr: Vec<Vec<bool>> = vec![vec![false; *n]; *n];
+        // let mut update_arr: Vec<[usize; 3]> = Vec::new();
         // let mut coordinates: Vec<&_> = Vec::with_capacity(n*n);
 
+        //
         let max: u8 = *n as u8;
         for y in 0..max {
             for x in 0..max {
@@ -26,6 +33,7 @@ impl Grid {
 
         // println!("{:?}", coordinates);
         // return Self { arr, coordinates };
+        // return Ok(Self { arr, update_arr, size: *n });
         return Ok(Self { arr, size: *n });
     }
 
@@ -33,12 +41,12 @@ impl Grid {
         // by gpt. i haven't actually looked at closures properly yet
         let input_str: String = input_str.chars().filter(|&c| !c.is_whitespace()).collect();
 
-        if input_str.len() != self.size * self.size {
-            return Err(format!(
-                "Starting state values must conatins {} values",
-                self.size * self.size
-            ));
-        }
+        // if input_str.len() != self.size * self.size {
+        //     return Err(format!(
+        //         "Starting state values must conatins {} values",
+        //         self.size * self.size
+        //     ));
+        // }
 
         let mut i: usize = 0;
         for row in &mut self.arr {
@@ -54,6 +62,76 @@ impl Grid {
         }
         Ok(())
     }
+
+    pub fn life(&mut self) -> Vec<[usize; 3]>{
+        let mut update_array: Vec<[usize; 3]> = Vec::new();
+        let mut x: usize;
+        let mut y: usize = 0;
+        // let arr = &self.arr;
+        for row in self.arr.iter() {
+            x = 0;
+            for c in row {
+                // let cell = *c;
+                // println!("Checking x: {}, y: {}", x, y);
+                let neighbours: u8 = self.alive_neighbours(x, y);
+                // println!("Alive neighbours: {}", neighbours);
+                let new_cell: bool = self.apply_rule(c, neighbours);
+                if new_cell != *c {
+                    update_array.push([x, y, if new_cell { 1 as usize } else { 0 as usize }]);
+                }
+                x += 1;
+            }
+            y += 1;
+        }
+
+        for update in update_array.iter(){
+            let _x: usize = update[0];
+            let _y: usize = update[1];
+            let _cell: bool = update[2] == 1;
+            self.arr[_y][_x] = _cell;
+        }
+        return update_array;
+    }
+
+    fn apply_rule(&self, cell: &bool, neighbours: u8) -> bool{
+        match *cell{
+            true => {
+                return (neighbours == 2) || (neighbours == 3)
+            }
+            false => {
+                return neighbours == 3
+            }
+        }
+    }
+
+    fn alive_neighbours(&self, x: usize, y: usize) -> u8 {
+        // use signed ints to check for negative values
+        let offset: [i8; 3] = [-1, 0, 1];
+        let offsets = |v: usize| {
+            let _v: i8 = v as i8;
+            return offset.clone().map(|o: i8| o + _v);
+        };
+
+        let x_values: [i8; 3] = offsets(x);
+        let y_values: [i8; 3] = offsets(y);
+
+        let mut sum: u8 = 0;
+
+        for _x in x_values {
+            for _y in y_values {
+                if (_x < 0 || // no negative coorindates
+                   _y < 0 ||
+                   _x as usize == self.size || // no overflow
+                   _y as usize == self.size ||
+                   _x as usize == x && _y as usize == y) { // skip the same coor as the current cell
+                    continue;
+                }
+                sum += self.arr[_y as usize][_x as usize] as u8;
+            }
+        }
+
+        return sum;
+    }
 }
 
 impl fmt::Display for Grid {
@@ -61,8 +139,8 @@ impl fmt::Display for Grid {
         let (x, y) = (-1, -1);
         for row in &self.arr {
             for cell in row {
-                write!(f, "{}", if *cell { 1 } else { 0 })?;
-                write!(f, " ")?;
+                write!(f, "{}", if *cell { 'X' } else { ' ' })?;
+                // write!(f, " ")?;
             }
             writeln!(f, " ")?;
         }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 #![allow(unused_mut)]
+#![allow(unused_parens)]
 
-use std::fmt;
+use std::thread::sleep;
+use std::{any, fmt};
 use std::io::{self, Write};
 use std::ptr::null;
 use std::time::Duration;
@@ -21,19 +23,26 @@ const X: &str = "X";
 fn main() {
     // clear();
     let starting_state = r#"
-    X X X
-    X X X
-    X X X
+    _ _ _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _ _ _
+    _ _ _ _ _ X _ _ _ _
+    _ _ _ X _ X _ _ _ _
+    _ _ _ _ X X _ _ _ _
+    _ _ _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _ _ _
     "#;
-    let grid = init(starting_state);
-    print!("{}", grid);
+    let mut grid: Grid = init(starting_state);
+    progress_life(&mut grid);
 }
 
 fn init(starting_state: &str) -> Grid{
     let mut grid: Grid;
     let mut array_size: usize = 0;
     for line in starting_state.lines(){
-        array_size = line.matches(X).count();
+        array_size = line.replace(" ", "").len();
         if array_size == 0{
             continue;
         }
@@ -51,9 +60,28 @@ fn init(starting_state: &str) -> Grid{
             panic!("Error is: {}", error)
         }
     }
-    // DON'T pass a reference
+    // DON'T pass a reference.
     // this is MOVING ownership from init() to the parent function
     return grid
 }
 
-fn progress_life(grid: &mut Grid) {}
+fn progress_life(grid: &mut Grid) {
+    clear();
+    print!("{}", grid);
+    loop{
+        // sleep(Duration::from_millis(40));
+        sleep(Duration::from_secs(1));
+        let update_array: Vec<[usize; 3]> = grid.life();
+        for update in update_array.iter(){
+            // println!("{}, {}", update[0], update[1]);
+            set_cursor_position(update[0], update[1]);
+            print!("=")
+        }
+        break;
+        clear();
+        print!("{}", grid);
+        if update_array.len() == 0{
+            break;
+        }
+    }
+}
